a75a0cf78ee93abac7793e9769c037769c6c1698
yangtory <wjdduscldrn@naver.com>
2024-01-25 13:10:58 +0900

    add.js수정

--- team_fridge/public/js/add.js
+++ team_fridge/public/js/add.js
@@ -1,11 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.querySelector("div.btn_box");
-
-  btn.addEventListener("click", (e) => {
-    const target = e.target;
-    const value = target.value;
-    if (value === "장바구니") {
-      alert();
-    }
-  });
-});
+document.addEventListener("DOMContentLoaded", () => {});
