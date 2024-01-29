ef6d66498f06825d1870f438ba088c452defbf44
WHM0304 <whm0304@naver.com>
2024-01-29 22:38:54 +0900

    js수정

--- team_fridge/routes/calendar.js
+++ team_fridge/routes/calendar.js
@@ -1,15 +1,34 @@
-import express, { json } from "express";
-
+import express from "express";
+import DB from "../config/mysql.js";
 const router = express.Router();
-
+const dbConn = DB.init();
 router.get("/", (req, res) => {
-  return res.render("./calendar/calendar.pug");
+  const sql = " SELECT * FROM tbl_item ";
+  dbConn.query(sql, (err, result) => {
+    if (err) {
+      return res.json(err);
+    } else {
+      if (result) {
+        return res.render("./calendar/calendar.pug", { result: result });
+      }
+    }
+  });
 });
 
 router.get(`/:day/detail`, (req, res) => {
   const row = req.params.day;
-
-  return res.render("./calendar/detail.pug", { row: [row] });
+  const params = [row];
+  const sql = " SELECT * FROM tbl_item WHERE i_date = ? ";
+  dbConn.query(sql, params, (err, result) => {
+    if (err) {
+      return res.json(err);
+    } else if (result) {
+      return res.render("./calendar/detail.pug", { row: params, result: result });
+    } else if (!result) {
+      alert("구매한 물품이없습니다.");
+      return res.redirect("./calendar/calendar.pug");
+    }
+  });
 });
 
 export default router;
