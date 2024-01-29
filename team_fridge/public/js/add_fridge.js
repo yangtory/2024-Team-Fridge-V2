cae58407d43ef6fc3575a451050df62a41fa7569
yangtory <wjdduscldrn@naver.com>
2024-01-29 13:48:18 +0900

    addfridge수정하기

--- team_fridge/public/js/add_fridge.js
+++ team_fridge/public/js/add_fridge.js
@@ -1,1 +1,39 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  F_INDEX = {
+    F_PHOTO: 0,
+    F_FRESH: 1,
+    F_COLD: 2,
+    F_OUT: 3,
+    F_NAME: 4,
+    F_MEMO: 5,
+  };
+
+  const f_photo = document.querySelector("#selectfile");
+  const f_fresh = document.querySelector("#fresh");
+  const f_cold = document.querySelector("#cold");
+  const f_out = document.querySelector("#out");
+  const f_name = document.querySelector("#f_name");
+  const f_memo = document.querySelector("#f_memo");
+  const form = document.querySelector("form.add");
+  const f_btn = document.querySelector("#f_btn");
+  const error_divs = document.querySelectorAll("div.add.error");
+  const select = document.querySelector("div.select");
+
+  select.addEventListener("click", (e) => {
+    const target = e.target;
+    const value = target.value;
+    if (value === "냉장") {
+      alert("클릭");
+    }
+  });
+
+  f_btn.addEventListener("click", () => {
+    error_divs.forEach((item) => item.innerHTML);
+    if (!f_name.value) {
+      error_divs[F_INDEX.F_NAME].innerHTML = "* 냉장고 이름을 입력하세요";
+      f_name.select();
+      return false;
+    }
+    form.submit();
+  });
+});
--- team_fridge/routes/fridge.js
+++ team_fridge/routes/fridge.js
@@ -1,4 +1,5 @@
 import express from "express";
+import dbCreate from "../config/mysql.js";
 
 const router = express.Router();
 
@@ -14,8 +15,24 @@ router.get("/shopmemo", (req, res) => {
   return res.render("fridge/shopmemo");
 });
 
-router.post("/frilist", (req, res) => {
-  return res.render("fridge/list_fridge");
+router.post("/add_fridge", (req, res) => {
+  const fr_photo = req.body.photo;
+  const fr_fresh = req.body.fresh;
+  const fr_cold = req.body.cold;
+  const fr_out = req.body.out;
+  const fr_name = req.body.f_name;
+  const fr_memo = req.body.f_memo;
+
+  const params = [fr_photo, fr_fresh, fr_cold, fr_out, fr_name, fr_memo];
+  console.log(params);
+  // const sql = "INSERT INTO tbl_fridge(fr_photo, fr_fresh, fr_cold, fr_out, fr_name, fr_memo) " + "VALUES (?,?,?,?,?,?)";
+  // dbCreate.query(sql, params, (err, result) => {
+  //   if (err) {
+  //     return res.json(err);
+  //   } else {
+  //     return res.render("/fridge/list_fridge");
+  //   }
 });
+// });
 
 export default router;
