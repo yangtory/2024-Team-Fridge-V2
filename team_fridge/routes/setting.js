f487e3fc62126bc192a7319e072766773c3ec6fd
loo919239 <loo919239@naver.com>
2024-01-30 13:03:56 +0900

    setting

--- team_fridge/routes/setting.js
+++ team_fridge/routes/setting.js
@@ -1,9 +1,41 @@
 import express from "express";
+import DB from "../setting_config/mysql.js";
 
 const router = express.Router();
 
+let dbConn = null;
+
+DB.init().then((connection) => {
+  dbConn = connection;
+});
+
 router.get("/", (req, res) => {
-  return res.render("/fridge/setting");
+  res.render("setting/setting");
+});
+
+router.get("/login", (req, res) => {
+  res.render("setting/login");
+});
+
+router.get("/join", (req, res) => {
+  res.render("setting/join");
+});
+
+router.post("/join", (req, res) => {
+  const sql = " INSERT INTO tbl_user SET ? ";
+  const params = {
+    username: req.body.username,
+    userid: req.body.userid,
+    password: req.body.password,
+  };
+  dbConn
+    .query(sql, params)
+    .then((_) => {
+      return res.redirect("/setting/login");
+    })
+    .catch((err) => {
+      return res.render(err);
+    });
 });
 
 export default router;
