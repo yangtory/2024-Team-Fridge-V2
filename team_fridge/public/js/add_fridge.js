bdae1da1098424615e396488468f3fb473b7c480
n96js <n96js@naver.com>
2024-01-30 13:50:46 +0900

    화면구현

--- team_fridge/public/js/add_fridge.js
+++ team_fridge/public/js/add_fridge.js
@@ -1,1 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const btn_add = document.querySelector("btn.add");
+  const food_detail = document.querySelector("div.food");
+  btn_add.addEventListener("click", () => {
+    document.location.href = "add_food";
+  });
+  food_detail.addEventListener("click", () => {
+    document.location.href = "fridge_detail";
+  });
+
+  //   const btn_update = document.querySelector("button.update");
+  //   const btn_delete = document.querySelector("button.delete");
+  //   const btn_list = document.querySelector("button.list");
+
+  //   btn_update.addEventListener("click", () => {
+  //     document.location.href = "update";
+  //   });
+  //   btn_delete.addEventListener("click", () => {
+  //     if (confirm("음식정보를 삭제할까요?")) {
+  //       document.location.href = "delete";
+  //     }
+  //   });
+  //   btn_list.addEventListener("click", () => {
+  //     document.location.href = "/alarm/fridge_list";
+  //   });
+});
--- team_fridge/routes/alarm.js
+++ team_fridge/routes/alarm.js
@@ -23,4 +23,16 @@ router.get("/fridge_list", (req, res) => {
   return res.render("alarm/fridge_list");
 });
 
+router.get("/fridge_detail", (req, res) => {
+  return res.render("alarm/fridge_detail");
+});
+
+router.get("/add_food", (req, res) => {
+  return res.render("alarm/add_food");
+});
+
+// router.post("/fridge_list/add_food", (req, res) => {
+//   return res.redirect("alarm/add_food");
+// });
+
 export default router;
