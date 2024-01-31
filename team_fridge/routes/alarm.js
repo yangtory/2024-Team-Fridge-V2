71524431b68cb7629d0ae3e2d2fa1d74e0158e70
n96js <n96js@naver.com>
2024-01-31 17:46:14 +0900

    js수정

--- team_fridge/routes/alarm.js
+++ team_fridge/routes/alarm.js
@@ -1,9 +1,17 @@
 import express from "express";
-
+import DB from "../config/mysql.js";
 const router = express.Router();
+const dbConn = DB.init();
 
 router.get("/", (req, res) => {
-  return res.render("alarm/alarm");
+  const sql = " SELECT * FROM tbl_food ";
+  dbConn.query(sql, (err, result) => {
+    if (err) {
+      return res.json(err);
+    } else {
+      return res.render("alarm/alarm.pug", { result: result });
+    }
+  });
 });
 
 // router.get("/:foodName/detail", async (req, res) => {
@@ -15,8 +23,18 @@ router.get("/", (req, res) => {
 //     return res.json(error);
 //   }
 // });
-router.get("/detail", (req, res) => {
-  return res.render("alarm/detail");
+router.get("/:p_num/detail", (req, res) => {
+  const f_num = req.params.p_num;
+  const sql = " SELECT * FROM tbl_food WHERE p_num = ? ";
+  const params = [f_num];
+  dbConn.query(sql, params, (err, result) => {
+    if (err) {
+      return res.json(err);
+    } else {
+      // return res.json(result);
+      return res.render("alarm/detail", { result: result[0] });
+    }
+  });
 });
 
 router.get("/add_food", (req, res) => {
