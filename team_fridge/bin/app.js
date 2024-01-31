d38e83f3b9debb03a48c93610d272221ff23db91
yangtory <wjdduscldrn@naver.com>
2024-01-31 15:49:24 +0900

    session

--- team_fridge/bin/app.js
+++ team_fridge/bin/app.js
@@ -11,6 +11,7 @@ import express from "express";
 import createError from "http-errors";
 import path from "path";
 import helmet from "helmet";
+import session from "express-session";
 
 // 3rd party lib modules
 import cookieParser from "cookie-parser";
@@ -44,6 +45,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join("public")));
 
+app.use(
+  session({
+    key: "fridge",
+    secret: "wjdduscldrn@naver.com",
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60,
+    },
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals = req.session;
+  next();
+});
 // router link enable, link connection
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
