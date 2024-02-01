182f402547e539bc3941a8972983b46f39f40431
loo919239 <loo919239@naver.com>
2024-02-01 17:50:47 +0900

    setting

---
+++ team_fridge/models/tbl_shopping.js
@@ -0,0 +1,36 @@
+import { Model } from "sequelize";
+
+export default class tbl_shopping extends Model {
+  static init(sequelize, DataTypes) {
+    return super.init(
+      {
+        s_num: {
+          type: DataTypes.STRING(125),
+          allowNull: false,
+          primaryKey: true,
+        },
+        s_name: {
+          type: DataTypes.STRING(125),
+          allowNull: false,
+        },
+        s_quan: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        tableName: "tbl_shopping",
+        timestamps: false,
+        indexes: [
+          {
+            name: "PRIMARY",
+            unique: true,
+            using: "BTREE",
+            fields: [{ name: "s_num" }],
+          },
+        ],
+      }
+    );
+  }
+}
