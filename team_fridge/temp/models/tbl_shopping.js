e536b743218feef8d93ef6423028a129def19f66
yangtory <wjdduscldrn@naver.com>
2024-02-05 13:19:52 +0900

    database수정

--- team_fridge/temp/models/tbl_shopping.js
+++ team_fridge/temp/models/tbl_shopping.js
@@ -1,36 +1,37 @@
-import _sequelize from 'sequelize';
-const { Model, Sequelize } = _sequelize;
+import _sequelize from "sequelize";
+const { Model } = _sequelize;
 
 export default class tbl_shopping extends Model {
   static init(sequelize, DataTypes) {
-  return super.init({
-    s_num: {
-      type: DataTypes.STRING(125),
-      allowNull: false,
-      primaryKey: true
-    },
-    s_name: {
-      type: DataTypes.STRING(125),
-      allowNull: false
-    },
-    s_quan: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    }
-  }, {
-    sequelize,
-    tableName: 'tbl_shopping',
-    timestamps: false,
-    indexes: [
+    return super.init(
       {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "s_num" },
-        ]
+        s_num: {
+          type: DataTypes.STRING(125),
+          allowNull: false,
+          primaryKey: true,
+        },
+        s_name: {
+          type: DataTypes.STRING(125),
+          allowNull: false,
+        },
+        s_quan: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
       },
-    ]
-  });
+      {
+        sequelize,
+        tableName: "tbl_shopping",
+        timestamps: false,
+        indexes: [
+          {
+            name: "PRIMARY",
+            unique: true,
+            using: "BTREE",
+            fields: [{ name: "s_num" }],
+          },
+        ],
+      }
+    );
   }
 }
--- team_fridge/temp/models/tbl_templist.js
+++ team_fridge/temp/models/tbl_templist.js
@@ -1,21 +1,24 @@
-import _sequelize from 'sequelize';
-const { Model, Sequelize } = _sequelize;
+import _sequelize from "sequelize";
+const { Model } = _sequelize;
 
 export default class tbl_templist extends Model {
   static init(sequelize, DataTypes) {
-  return super.init({
-    t_name: {
-      type: DataTypes.STRING(125),
-      allowNull: false
-    },
-    t_quan: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    }
-  }, {
-    sequelize,
-    tableName: 'tbl_templist',
-    timestamps: false
-  });
+    return super.init(
+      {
+        t_name: {
+          type: DataTypes.STRING(125),
+          allowNull: false,
+        },
+        t_quan: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        tableName: "tbl_templist",
+        timestamps: false,
+      }
+    );
   }
 }
