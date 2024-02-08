f4e0822fc99ac2c710bc62658caabd387b8bfe0e
n96js <n96js@naver.com>
2024-02-08 15:29:46 +0900

    유통기한별로나옴

--- team_fridge/routes/alarm.js
+++ team_fridge/routes/alarm.js
@@ -1,23 +1,24 @@
 import express from "express";
 import DB from "../models/index.js";
+import moment from "moment";
+import { Op } from "sequelize";
 const router = express.Router();
-// const dbConn = DB.init();
+
 const FRIDGE = DB.models.tbl_fridge;
 const PRODUCT = DB.models.tbl_product;
 
 router.get("/", async (req, res) => {
-  // const sql = " SELECT * FROM tbl_product ";
-  // dbConn.query(sql, (err, result) => {
-  //   if (err) {
-  //     return res.json(err);
-  //   } else {
-  //     return res.render("alarm/alarm.pug", { result: result });
-  //   }
-  // });
+  const today = moment().format("YYYY-MM-DD");
+  const exdate = moment().add(7, "days").format("YYYY-MM-DD");
 
   try {
-    const rows = await PRODUCT.findAll();
-
+    const rows = await PRODUCT.findAll({
+      where: {
+        p_exdate: { [Op.lte]: exdate },
+      },
+      order: [["p_exdate", "ASC"]],
+    });
+    // return res.json(rows);
     return res.render("alarm/alarm", { PRODUCT: rows });
   } catch (error) {
     return res.json(error);
@@ -36,9 +37,7 @@ router.get("/", async (req, res) => {
 router.get("/:p_seq/detail", async (req, res) => {
   const p_seq = req.params.p_seq;
   try {
-    const row = await PRODUCT.findByPk(p_seq, {
-      include: { model: FRIDGE, as: "tbl_fridges", include: { model: PRODUCT, as: "f_pseq_tbl_product" } },
-    });
+    const row = await PRODUCT.findByPk(p_seq);
     // return res.json(row);
     // console.log(row);
     return res.render("alarm/detail", { PRODUCT: row });
@@ -68,9 +67,7 @@ router.get("/:p_seq/delete", async (req, res) => {
 
 router.get("/:p_seq/update", async (req, res) => {
   const p_seq = req.params.p_seq;
-  const row = await PRODUCT.findByPk(p_seq, {
-    include: { model: FRIDGE, as: "tbl_fridges", include: { model: PRODUCT, as: "f_pseq_tbl_product" } },
-  });
+  const row = await PRODUCT.findByPk(p_seq);
   return res.render("fridge/add_food", { PRODUCT: row });
 });
 router.post("/:p_seq/update", async (req, res) => {
