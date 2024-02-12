643e19d395c2a90780097c1eb6d9917c5b9d20e2
yangtory <wjdduscldrn@naver.com>
2024-02-13 05:32:58 +0900

    자잘한수정

--- team_fridge/public/js/delete_fridge.js
+++ team_fridge/public/js/delete_fridge.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const fridge = document.querySelector('div.fridge');
     const btn_delete = document.querySelector('div.btn');
 
+    fridge.addEventListener('click', (e) => {});
+
     btn_delete.addEventListener('click', (e) => {
         const target = e.target;
         if (target.tagName === 'A') {
