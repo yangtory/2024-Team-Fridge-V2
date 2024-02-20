9527a0dc2fee7e39a55cd1369a408feccb0331e8
hykim6856 <hykim6856@naver.com>
2024-02-20 17:16:35 +0900

    주석청소

--- team_fridge/public/js/shopsave.js
+++ team_fridge/public/js/shopsave.js
@@ -5,10 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   newf.addEventListener("click", () => {
     document.location.href = "/fridge/add_fridge";
-    // location.replace("/fridge/add_fridge");
   });
   back.addEventListener("click", () => {
-    // document.location.href = "/fridge/shopmemo";
     location.replace("/fridge/shopmemo");
   });
 
@@ -17,11 +15,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const ul = target.closest("UL");
     const f_seq = ul.dataset.num;
-    // alert(f_seq);
     if (confirm("이 냉장고에 저장할까요?")) {
       alert("저장되었습니다.");
       location.replace(`/fridge/shopmemo/${f_seq}/save`);
-      // location.replace(`/${f_seq}/save`);
     }
   });
 });
